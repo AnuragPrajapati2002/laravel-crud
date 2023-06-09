aa9f7dd6acbefca57c25d2c68614ed55af8bf1f4
Anurag <manyasurve91102@gmail.com>
2023-06-09 18:54:06 +0530

    9junecommit

--- resources/views/arrayfunction.blade.php
+++ resources/views/arrayfunction.blade.php
@@ -327,7 +327,198 @@ function mark($x)
 
 $a=array("Anurag","Arjun","Mukesh");
 print_r(array_map("mark",$a));
+echo "<br>";
+
+// ---------------------------------------------------9/6/2023--------------------------------------------------
+//range()
+$result = range(3,17,2);
+print_r($result);
+echo "<br>";
 
 
+//range() with increment 10
+$a = range(10,50,5);
+print_r($a);
+echo "<br>";
+
+
+// arsort()
+$a = array("a"=>"45","b"=>"73","c"=>"09","d"=>"19");
+arsort($a);
+print_r($a);
+echo "<br>";
+
+
+//asort()
+$a = array("a"=>"45","b"=>"73","c"=>"-9","d"=>"19");
+asort($a);
+print_r($a);
+echo "<br>";
+
+
+//array_fill()
+$result = array_fill(3,4,"Blue");
+print_r($result);
+echo "<br>";
+
+
+
+//array_slice()
+$array = array("red","yellow","blue","green");
+$result = array_slice($array,1,2);
+print_r($result);
+echo "<br>";
+
+
+//array_slice() with preserve=true
+$a = array("red","yellow","green","blue");
+$result = array_slice($a,1,2,true);
+print_r($result);
+echo "<br>";
+
+$a1 = array("Red","Yellow","Blue");
+$a2 = array("Change Green", "Change Pink");
+array_splice($a1,0,2,$a2);
+print_r($a1);
+echo "<br>";
+
+
+
+//shuffle
+$m1 = array("Raja","Baja","Khaja","Maaja");
+shuffle($m1);
+print_r($m1);
+echo "<br>";
+
+
+
+ //array_map()
+function map($data)
+{
+    if($data ==="Java")
+    {
+    return "Springboot";
+    }
+    else if($data === "PHP")
+    {
+    return "Laravel";
+    }
+    return "Default";
+}
+ 
+$array = array("Java","PHP","Python");
+$result = array_map("map",$array);
+print_r($result);   
+echo "<br>";
+
+
+//krsort()
+$k = array("2"=>"Mukkadar","-3"=>"Ka","5"=>"Sikkdar");
+krsort($k);
+print_r($k);
+echo "<br>";
+
+//ksort()
+
+$k = array("8"=>"Raja","3"=>"Ram","19"=>"MOhan","2"=>"Roy");
+ksort($k);
+print_r($k);
+echo "<br>";
+
+
+//uasort
+function mysort($a,$b)
+{
+    if($a === $b)
+    {
+        return 0;
+    }
+    return ($a>$b)?1:-1;
+}
+$a = array("a"=>"Anurag","b"=>"Zebra","x"=>"Venom","y"=>"Bash");
+uasort($a,"mysort");
+print_r($a);
+echo "<br>";    
+
+
+//uasort
+function mysorts($a,$b)
+{
+    if($a === $b)
+    {
+        return 0;
+    }
+    return ($a>$b)?1:-1;
+}
+$a = array("m"=>"Anuradha","a"=>"Zimba","x"=>"Vulture","y"=>"Hash");
+uksort($a,"mysorts");
+print_r($a);
+echo "<br>";    
+
+//array_multisort()
+$a1 = array(45,78,23,89,67);
+$a2 = array(89,27,13,17,19);
+array_multisort($a1,SORT_DESC,SORT_NUMERIC,$a2,SORT_ASC);
+print_r($a1);
+echo "<br>";
+print_r($a2);
+echo "<br>";
+
+
+//array_search()
+
+$a = array("blue","moon","sun","star","5");
+if(array_search("star",$a,true))
+{
+    echo "Found";
+}
+else
+{
+    echo "Not found";
+}
+echo "<br>";
+
+
+//array_walk
+function walking($value,$key)
+{
+    echo $key."<br>";
+   echo "The key ".$key." has the value ".$value."<br>";
+}
+$walk = array("a"=>"Hum","x"=>"Prem","v"=>"Dijale","m"=>"Naseeb");
+array_walk($walk,"walking");
+echo"<br>";
+
+
+//current 
+$a = array("raster","linear","regression","momentum");
+echo current($a)."<br>";
+echo next($a)."<br>";
+echo prev($a)."<br>";
+echo end($a);
+echo"<br>";
+
+$people = array("Pushpa", "Jhon", "Glora", "Cluster");
+
+echo current($people)."<br>";
+echo next($people)."<br>";
+echo current($people)."<br>";
+echo reset($people);
+echo"<br>";
+
+
+//each
+$e = array("c"=>"Rakesh","d"=>"Vinod","a"=>"Lalit","b"=>"Parmod");
+print_r(each($e));
+echo"<br>";
+
+
+//compact()
+$name = "Anurag";
+$lname = "Prajapati";
+$age = "21";
+
+$result = compact("name","lname","age");
+print_r($result);
 ?>
 
